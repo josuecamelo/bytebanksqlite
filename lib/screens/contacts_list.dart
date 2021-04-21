03c72f7acf737282e8b28e93b5085d41828d5c7a
Josué Camelo <josueprg@gmail.com>
2021-04-20 23:21:52 -0300

    Alteração LIstagem de items

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,26 +1,20 @@
+import 'package:bytebankbd/models/contact.dart';
 import 'package:bytebankbd/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
+  final List<Contact> contacts = List();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Contatos')),
-      body: ListView(
-        children: [
-          Card(
-            child: ListTile(
-              title: Text(
-                'Josué',
-                style: TextStyle(fontSize: 24.0),
-              ),
-              subtitle: Text(
-                '1234',
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),
-          )
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index){
+          final Contact contact = contacts[index];
+          return _ContactItem(contact);
+        },
+        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -31,6 +25,28 @@ class ContactsList extends StatelessWidget {
               );
         },
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget{
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(fontSize: 24.0),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(fontSize: 16.0),
+        ),
       ),
     );
   }
